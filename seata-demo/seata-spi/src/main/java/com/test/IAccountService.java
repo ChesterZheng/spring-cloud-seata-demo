ef05fe134534f902e90570451ddf2f247cec29b2
xiaochen.zheng@2ccm.com <xiaochen.zheng@2ccm.com>
2020-10-13 15:30:22 +0800

    reset

--- seata-demo/seata-spi/src/main/java/com/test/IAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-import com.test.domain.Account;
-
-public interface IAccountService {
-
-	public boolean insertAccount(Long storeId, Account account) throws Exception;
-
-}
--- seata-demo/seata-spi/src/main/java/com/test/IOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-import com.test.domain.Order;
-
-public interface IOrderService {
-
-	public boolean insertOrder(Long storeId, Order order) throws Exception;
-
-}
